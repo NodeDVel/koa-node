85b19838778772f3ad872389114c26a59ce16b88
rlalasrl <rlaalsrl0810@naver.com>
2020-07-03 08:55:05 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Koa from 'koa';
+const app = new Koa();
+
+app.use(ctx => {
+  ctx.body = 'Hello Koa';
+});
+
+app.listen(4000, () => {
+  console.log('heurm server is listening to port 4000');
+})
