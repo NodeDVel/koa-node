88e3c7e1e1574a4904f5a6b4064c94fc08b26767
rlalasrl <rlaalsrl0810@naver.com>
2020-08-04 09:24:02 +0900

    first kor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import Koa from 'koa';
+import Router from 'koa-router';
+
 const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = '홈';
+});
 
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('heurm server is listening to port 4000');
